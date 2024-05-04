58c7b4a972c6f0d465fc1c974fea5c767911c7bd
RAYHAN-MU <rayhanahmed53432@gmail.com>
2024-05-04 12:50:33 +0600

    i modified three file and add button system

--- signin-up.js
+++ signin-up.js
@@ -1,16 +1,16 @@
 let logIn = document.getElementById("log");     
     let signupDiv1 = document.getElementById("sign"); 
 
-let logInbtn = document.getElementById("loginButton");     
-    let signupDiv1btn = document.getElementById("signupButton");
+let logInbtn1 = document.getElementById("loginButton1");     
+    let signupDiv1btn = document.getElementById("signupButton"); 
     signupDiv1btn.addEventListener("click", function() { 
         logIn.style.display = "none"; 
         signupDiv1.style.display = "block"; 
-        signupDiv1btn.classList.add("abcde"); 
+
+         
     }); 
-    logInbtn.addEventListener("click", function() {
-        signupDiv1.style.display = "none"; 
-      
+     logInbtn1.addEventListener("click", function() {
+        signupDiv1.style.display = "none";    
         logIn.style.display = "block"; 
-    });
+     });
    
