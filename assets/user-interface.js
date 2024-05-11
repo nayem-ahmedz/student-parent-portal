e4afd929a55094a65e5c7162fea54b853e547542
nayem-ahmedz <nayemahmed2n@gmail.com>
2024-05-11 22:35:55 +0600

    Updated page: user-interface

--- assets/user-interface.js
+++ assets/user-interface.js
@@ -14,9 +14,19 @@ function myFun(tV, event){
 function showPanel(){
     left = document.getElementById('ui-LEFT');
     left.style.width = '200px';
-    left.style.backgroundColor = 'white';
 }
 function closePanel(){
     left = document.getElementById('ui-LEFT');
     left.style.width = '0';
+}
+let open = true;
+function hideUserNav(){
+    let heighT = document.getElementById('user-nav-x');
+    if(!open){
+        heighT.style.height = '0';
+        open = true;
+    } else{
+        heighT.style.height = '110px';
+        open = false;
+    }
 }
