c28143227b3a504d776c478e71f611c3d43f85a4
nayem-ahmedz <nayemahmed2n@gmail.com>
2024-05-13 00:46:18 +0600

    Made header and nav responsive for smaller device

--- script.js
+++ script.js
@@ -2,3 +2,15 @@
 function regiFun(){
     document.location = '/re-nayem/signin-up.html?signup=true';
 }
+let navOpen = false;
+function myFunction(x) {
+    x.classList.toggle("change");
+    let a = document.getElementById('nav-contents');
+    if(navOpen==false){
+        a.style.height = 'auto';
+        navOpen = true;
+    } else{
+        a.style.height = 0;
+        navOpen = false;
+    }
+  }
